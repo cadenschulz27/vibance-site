8611f834c2152796b8b4a27b1003d9d400721771
cadenschulz27 <cjschulz@butler.edu>
2025-09-01 14:24:50 -0400

    Add files via upload

--- netlify/functions/getNews.js
+++ netlify/functions/getNews.js
@@ -1,20 +1,34 @@
 // FILE: netlify/functions/getNews.js
 
-// Using node-fetch to make the API request on the server
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-  // Get the secret API key from the environment variables
   const apiKey = process.env.NEWS_API_KEY;
-  const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=3&apiKey=${apiKey}`;
+  const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=10&apiKey=${apiKey}`;
+
+  // *** NEW: A list of sources to exclude ***
+  const paywalledSources = [
+    'The Wall Street Journal',
+    'Bloomberg',
+    'Reuters',
+    'Financial Times',
+    'The New York Times',
+    'The Economist'
+  ];
 
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
+    // *** NEW: Filter the articles to exclude paywalled sources ***
+    if (data.articles) {
+      const freeArticles = data.articles.filter(article => !paywalledSources.includes(article.source.name));
+      data.articles = freeArticles;
+    }
+
     return {
       statusCode: 200,
-      body: JSON.stringify(data), // Send the data back to the browser
+      body: JSON.stringify(data), // Send the filtered data back to the browser
     };
   } catch (error) {
     return {
