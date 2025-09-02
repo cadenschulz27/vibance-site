01f02ebd83249426aae68cf208c7108756013549
cadenschulz27 <cjschulz@butler.edu>
2025-09-02 14:45:00 -0400

    Add files via upload

--- public/api/firebase.js
+++ public/api/firebase.js
@@ -1,4 +1,4 @@
-// FILE: api/firebase.js
+// FILE: public/api/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
@@ -8,15 +8,14 @@ const firebaseConfig = {
     apiKey: "AIzaSyC4yxIaK9WDTwrk1riHfYxAvq9Z_v7wGDM",
     authDomain: "vibanceco.firebaseapp.com",
     projectId: "vibanceco",
-    // THE FIX IS HERE: Updated to the correct bucket name
-    storageBucket: "vibanceco.firebasestorage.app",
+    storageBucket: "vibanceco.appspot.com",
     messagingSenderId: "1068160687178",
     appId: "1:1068160687178:web:3cbff24fe03c98fc521ab3"
 };
 
-const app = initializeApp(firebaseConfig);
+// CHANGED: Added 'export' so other files can import the main app object
+export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
