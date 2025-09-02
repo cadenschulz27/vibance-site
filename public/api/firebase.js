36e4665a16d0d10def3d544fe20a13c89ffe7ce8
cadenschulz27 <cjschulz@butler.edu>
2025-09-02 15:04:37 -0400

    Add files via upload

--- public/api/firebase.js
+++ public/api/firebase.js
@@ -8,14 +8,13 @@ const firebaseConfig = {
     apiKey: "AIzaSyC4yxIaK9WDTwrk1riHfYxAvq9Z_v7wGDM",
     authDomain: "vibanceco.firebaseapp.com",
     projectId: "vibanceco",
-    storageBucket: "vibanceco.appspot.com",
+    // THE FIX IS HERE: Updated to the correct bucket name
+    storageBucket: "vibanceco.firebasestorage.app", 
     messagingSenderId: "1068160687178",
     appId: "1:1068160687178:web:3cbff24fe03c98fc521ab3"
 };
 
-// CHANGED: Added 'export' so other files can import the main app object
 export const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
