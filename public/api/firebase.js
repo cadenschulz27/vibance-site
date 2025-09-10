14e5d81f45155ce0d839ff291a7ee8b61a968f55
Caden <cjschulz@butler.edu>
2025-09-10 15:40:19 -0400

    config update

--- public/api/firebase.js
+++ public/api/firebase.js
@@ -5,13 +5,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC4yxIaK9WDTwrk1riHfYxAvq9Z_v7wGDM",
-    authDomain: "vibanceco.firebaseapp.com",
-    projectId: "vibanceco",
-    storageBucket: "vibanceco.appspot.com", 
-    messagingSenderId: "1068160687178",
-    appId: "1:1068160687178:web:3cbff24fe03c98fc521ab3"
+  apiKey: "AIzaSyC4yxIaK9WDTwrk1riHfYxAvq9Z_v7wGDM",
+  authDomain: "vibanceco.firebaseapp.com",
+  projectId: "vibanceco",
+  storageBucket: "vibanceco.firebasestorage.app",
+  messagingSenderId: "1068160687178",
+  appId: "1:1068160687178:web:3cbff24fe03c98fc521ab3",
+  measurementId: "G-T9L0S3VSKP"
 };
 
 // Initialize and export Firebase services
